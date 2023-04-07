8b33a731b8d6e4589c14d15359273e1c555aa44a
unknown <sharnikhil2003@gmail.com>
2023-04-07 18:26:31 +0530

    first upload

---
+++ js/resp.js
@@ -0,0 +1,10 @@
+burger = document.querySelector('.burger')
+navbar = document.querySelector('.navbar')
+navlist = document.querySelector('.navlist')
+searchlist = document.querySelector('.searchlist')
+
+burger.addEventListner('click', ()=>{
+    navbar.classlist.toggle('h-class');
+    searchlist.classlist.toggle('v-class');
+    navlist.classlist.toggle('v-class');
+})
